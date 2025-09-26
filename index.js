1ccb418b9810c4ba1a546ed5d460c6605086dc88
Tanay-Bankar-001 <tanaybankar001@gmail.com>
2025-09-26 14:50:08 +0530

    Connect backend to MongoDB Atlas

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+//connecting mongoDB
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB Atlas!'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+
 const express = require('express'); //import express library
 const app = express();  //create express app (server)
 
